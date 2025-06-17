c7bde757714a859a30c6994ab5d103b805a8b41d
yoobe-devs <33420851+yoobe-devs@users.noreply.github.com>
2025-06-16 21:51:54 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
-
 const express = require('express');
 const app = express();
 const routes = require('./routes');
 require('dotenv').config();
 
 app.use(express.json());
+
+// Rota raiz da aplicação
+app.get('/', (req, res) => {
+  res.send('🧠 Yoobe API está online! Acesse /api para mais informações.');
+});
+
+// Todas as outras rotas continuam em /api
 app.use('/api', routes);
 
 const PORT = process.env.PORT || 3000;
